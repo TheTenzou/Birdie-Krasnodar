e6a17929de57e715b777908038b8e6fd62a785d4
Tenzou <61962705+TheTenzou@users.noreply.github.com>
2020-09-22 17:46:17 +0300

    Add database credentials

--- restaurant_information_portal/settings.py
+++ restaurant_information_portal/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'restaurant_information_portal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        # TODO: use enviroment variables for database credentials
+        'NAME': 'restaurant-information-portal-database',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'POST': '5432'
     }
 }
 
