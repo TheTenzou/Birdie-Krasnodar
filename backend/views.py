ecc882fc8b45f4900cead6529c288d8682702bba
Tenzou <61962705+TheTenzou@users.noreply.github.com>
2020-09-30 12:07:09 +0300

    Add create user

--- backend/views.py
+++ backend/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import status, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .serializers import UserSerialier
+
 
 # Create your views here.
+class UserCreate(APIView):
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format='json'):
+        serializer = UserSerialier(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            if user:
+                json = serializer.data
+                return Response(json, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
