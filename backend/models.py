7e735f14d5ba7c15519db80fbab005f19b1cf09d
Tenzou <61962705+TheTenzou@users.noreply.github.com>
2020-10-01 15:21:54 +0300

    Add restarant model

--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,60 @@
 from django.contrib.auth.models import AbstractUser
-from django.db import models
+from django.contrib.gis.db import models
 
 
 # Create your models here.
 class User(AbstractUser):
-    profile_picture = models.ImageField(upload_to='file_storage/profile_pictures')
+    profile_picture = models.ImageField(
+        upload_to='file_storage/profile_pictures', 
+        blank=True, 
+        null=True
+        )
+
+
+class Price_bracket(models.Model):
+    name = models.CharField(max_length=20)
+
+
+class Food_type(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=20, help_text='Название ресторана.')
+    description = models.CharField(max_length=1000, help_text='Описание ресторана.')
+    rating = models.FloatField(blank=True, null=True)
+    food_type = models.ManyToManyField(Food_type)
+    card_picture = models.ImageField(
+        upload_to='file_storage/restrant_card_picture', 
+        blank=True, 
+        null=True
+        )
+
+
+    def __str__(self):
+        return self.name
+
+
+class Address(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    address = models.CharField(max_length=100)
+    coordinates = models.PointField()
+
+
+class Restaurant_picture(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    picture = models.ImageField(
+        upload_to='file_storage/restaurant_pictures', 
+        blank=True, 
+        null=True
+        )
+
+
+class Food_picture(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    picture = models.ImageField(
+        upload_to='file_storage/food_pictures', 
+        blank=True, 
+        null=True
+        )
+
