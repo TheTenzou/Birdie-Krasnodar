9835541b954c6dffc4047113436aa213803e2cd4
Tenzou <61962705+TheTenzou@users.noreply.github.com>
2020-10-02 20:03:40 +0300

    Add models for news, reviews, comments

--- backend/models.py
+++ backend/models.py
@@ -8,7 +8,7 @@ class User(AbstractUser):
         upload_to='file_storage/profile_pictures', 
         blank=True, 
         null=True
-        )
+    )
 
 
 class Price_bracket(models.Model):
@@ -40,7 +40,7 @@ class Restaurant(models.Model):
         upload_to='file_storage/restrant_card_picture', 
         blank=True, 
         null=True
-        )
+    )
 
     class Meta:
         ordering = ['name']
@@ -61,14 +61,51 @@ class Restaurant_picture(models.Model):
         upload_to='file_storage/restaurant_pictures', 
         blank=True, 
         null=True
-        )
+    )
 
 
 class Food_picture(models.Model):
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
-    picture = models.ImageField(
-        upload_to='file_storage/food_pictures', 
-        blank=True, 
-        null=True
-        )
+    picture = models.ImageField(upload_to='file_storage/food_pictures')
 
+
+class News(models.Model):
+    headline = models.CharField(max_length=100)
+    news_text = models.CharField(max_length=1000)
+    publication_date = models.DateTimeField(auto_now_add=True)
+    card_picture = models.ImageField(
+        upload_to='file_storage/news_picture_card',
+        blank=True,
+        null=True
+    )
+
+    class Meta():
+        ordering = ['headline']
+
+    def __str__(self):
+        return self.headline
+    
+class News_picture(models.Model):
+    news = models.ForeignKey(News, on_delete=models.CASCADE)
+    picture = models.ImageField(upload_to='file_storage/news_pictures')
+
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    up_voite_users = models.ManyToManyField(User, related_name='up_voite_users')
+    down_voite_users = models.ManyToManyField(User, related_name='down_voite_users')
+    headline = models.CharField(max_length=100)
+    text = models.CharField(max_length=500)
+
+    class Meta():
+        ordering = ['headline']
+    
+    def __str__(self):
+        return self.headline
+
+    
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    news = models.ForeignKey(News, on_delete=models.CASCADE)
+    text = models.CharField(max_length=500)
