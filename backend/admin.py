c13e9942d45f6980155cbc1923fd2e5399cc234a
Tenzou <61962705+TheTenzou@users.noreply.github.com>
2020-10-04 19:07:38 +0300

    Add News in admin site

--- backend/admin.py
+++ backend/admin.py
@@ -1,10 +1,47 @@
 from django.contrib import admin
-from .models import User
+from . import models
 
 
 # Register your models here.
 class UserAdmin(admin.ModelAdmin):
-    model = User
+    model = models.User
 
 
-admin.site.register(User, UserAdmin)
+class RestaurantPicturesInLine(admin.TabularInline):
+    model = models.Restaurant_picture
+    extra = 0
+
+
+class FoodPicturesInLine(admin.TabularInline):
+    model = models.Food_picture
+    extra = 0
+
+
+class AddressInLine(admin.TabularInline):
+    model = models.Address
+    extra = 0
+
+
+class RestaurantAdmin(admin.ModelAdmin):
+    inlines = [
+        RestaurantPicturesInLine, 
+        FoodPicturesInLine,
+        AddressInLine # TODO fix map
+    ]
+
+
+class NewsPictures(admin.TabularInline):
+    model = models.News_picture
+    extra = 0
+
+
+class NewsAdmin(admin.ModelAdmin):
+    fields = ('headline', 'news_type', 'news_text', 'card_picture')
+    inlines = [
+        NewsPictures,
+    ]
+
+
+admin.site.register(models.User, UserAdmin)
+admin.site.register(models.Restaurant, RestaurantAdmin)
+admin.site.register(models.News, NewsAdmin)
