193c138117e70a4e47e93f1baf3e9a29675b99e5
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2020-10-11 21:00:48 +0300

    Add nav bar

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -7,6 +7,11 @@ class App extends Component{
     render(){
         return(
             <div className="site">
+                <nav>
+                    <Link className={"nav-link"} to={"/"}>Home</Link>
+                    <Link className={"nav-link"} to={"/login/"}>Login</Link>
+                    <Link className={"nav-link"} to={"/signup/"}>Signup</Link>
+                </nav>
                 <main>
                     <h1>Ahhh after 10,000 years I'm free. Time to conquer the Earth!</h1>
                     <Switch>
