fdaed268cb31165958ebaea6b7f125312e5cea79
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2020-10-11 20:38:00 +0300

    Add dummy login,  singup, home pages

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,10 +1,20 @@
 import React, { Component} from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import Login from "./login";
+import Signup from "./singup";
 
 class App extends Component{
     render(){
         return(
             <div className="site">
-                <h1>Ahhh after 10,000 years I'm free. Time to conquer the Earth!</h1>
+                <main>
+                    <h1>Ahhh after 10,000 years I'm free. Time to conquer the Earth!</h1>
+                    <Switch>
+                        <Route exact path={"/login/"} component={Login}/>
+                        <Route exact path={"/signup/"} component={Signup}/>
+                        <Route exact path={"/"} render={() => <div>Home page</div>}/>
+                    </Switch>
+                </main>
             </div>
         );
     }
---
+++ frontend/src/components/singup.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Signup extends Component{
+    constructor(prop){
+        super(prop)
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Signup page</h2>
+            </div>
+        )
+    }
+}
+export default Signup;
