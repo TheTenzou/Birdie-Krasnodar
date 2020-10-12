d4d22f40ce19568d7a0f68045a13eda18e8dc038
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2020-10-12 11:09:05 +0300

    Add hello page but its broken

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -2,6 +2,7 @@ import React, { Component} from "react";
 import { Switch, Route, Link } from "react-router-dom";
 import Login from "./login";
 import Signup from "./signup";
+import Hello from "./hello";
 
 class App extends Component{
     render(){
@@ -11,12 +12,14 @@ class App extends Component{
                     <Link className={"nav-link"} to={"/"}>Home</Link>
                     <Link className={"nav-link"} to={"/login/"}>Login</Link>
                     <Link className={"nav-link"} to={"/signup/"}>Signup</Link>
+                    <Link className={"nav-link"} to={"/hello/"}>Hello</Link>
                 </nav>
                 <main>
                     <h1>Ahhh after 10,000 years I'm free. Time to conquer the Earth!</h1>
                     <Switch>
                         <Route exact path={"/login/"} component={Login}/>
                         <Route exact path={"/signup/"} component={Signup}/>
+                        <Route exact path={"/hello/"} component={Hello}/>
                         <Route exact path={"/"} render={() => <div>Home page</div>}/>
                     </Switch>
                 </main>
