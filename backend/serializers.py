1834e317b71b8a78acc2ffd0ca24909d54834d09
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2020-11-06 18:16:04 +0300

    Add restaurant serializer

--- backend/serializers.py
+++ backend/serializers.py
@@ -1,8 +1,9 @@
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework import serializers
-from .models import User
+from .models import User, Restaurant, Price_bracket
 
 
+# Сериалезатор для пользователя
 class UserSerialier(serializers.ModelSerializer):
 
     email = serializers.EmailField(
@@ -11,17 +12,23 @@ class UserSerialier(serializers.ModelSerializer):
     username = serializers.CharField()
     password = serializers.CharField(min_length=8, write_only=True)
 
-
     class Meta:
         model = User
         fields = ('email', 'username', 'password')
         extra_kwargs = {'password': {'write_only': True}}
 
-    
     def create(self, validated_data):
         password = validated_data.pop('password', None)
-        instance = self.Meta.model(**validated_data) # TODO redo
+        instance = self.Meta.model(**validated_data)  # TODO redo
         if password is not None:
             instance.set_password(password)
         instance.save()
         return instance
+
+
+# Сериалезаторо ресторана
+class RestaurantSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Restaurant
+        fields = ('id', 'description', 'food_type', 'price_bracket', 'card_picture')
--- backend/views.py
+++ backend/views.py
@@ -4,10 +4,12 @@ from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .serializers import UserSerialier
-
+from .serializers import UserSerialier, RestaurantSerializer
+from .models import Restaurant
 
 # Create your views here.
+
+
 class UserCreate(APIView):
     permission_classes = (permissions.AllowAny,)
 
@@ -19,3 +21,12 @@ class UserCreate(APIView):
                 json = serializer.data
                 return Response(json, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class RestaurantList(APIView):
+    permission_classes = (permissions.AllowAny,)
+
+    def get(self, request):
+        restaurants = Restaurant.objects.all()
+        serializer = RestaurantSerializer(restaurants, many=True)
+        return Response(serializer.data)
