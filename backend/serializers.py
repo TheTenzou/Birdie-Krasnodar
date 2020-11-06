a5c5deac05bea3d67a4f25581cf376506101b489
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2020-11-06 18:24:46 +0300

    Add serializer for food and price

--- backend/serializers.py
+++ backend/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework import serializers
-from .models import User, Restaurant, Price_bracket
+from .models import User, Restaurant, Price_bracket, Food_type
 
 
 # Сериалезатор для пользователя
@@ -25,6 +25,21 @@ class UserSerialier(serializers.ModelSerializer):
         instance.save()
         return instance
 
+
+# Сериализатор ценовых категорий
+class PriceBracketSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Price_bracket
+        field = ('id', 'name')
+
+
+# Сериализатор типов еды
+class FoodTypeSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Food_type
+        fields = ('id', 'name')
 
 # Сериалезаторо ресторана
 class RestaurantSerializer(serializers.ModelSerializer):
