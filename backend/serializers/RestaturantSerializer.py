14b5c70c8ca85c58ff9d0850b93c476cb1cc152c
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2020-11-07 23:09:40 +0300

    Add rating to restaurant api

--- backend/serializers/RestaturantSerializer.py
+++ backend/serializers/RestaturantSerializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from ..models import Restaurant
+from ..models import Restaurant, RestaurantRating
 from ..serializers import PriceBracketSerializer, FoodTypeSerializer
 
 # Сериалезаторо ресторана
@@ -7,7 +7,16 @@ class RestaurantSerializer(serializers.ModelSerializer):
     price_bracket = PriceBracketSerializer(many=False, read_only=True)
     food_type = FoodTypeSerializer(many=True, read_only=True)
     card_picture = serializers.FileField(max_length=None, use_url=True, allow_null=True, required=False)
+    rating = serializers.SerializerMethodField()
 
     class Meta:
         model = Restaurant
-        fields = ('id', 'description', 'food_type', 'price_bracket', 'card_picture')
+        fields = ('id', 'description', 'food_type', 'price_bracket', 'card_picture', 'rating')
+    
+    def get_rating(self, obj):
+        ratings = list(RestaurantRating.objects.filter(restaurant=obj.id))
+
+        summ = 0
+        for rating in ratings:
+            summ += rating.rating
+        return summ / len(ratings)
