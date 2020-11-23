03ce9371282c5bd49ef7ab89c013f493982fd33a
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2020-11-23 09:05:29 +0300

    Add filters, ordering, search to news

--- backend/views/News/NewsList.py
+++ backend/views/News/NewsList.py
@@ -1,19 +1,28 @@
 from rest_framework import status, permissions
-from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.generics import ListAPIView
+from rest_framework.filters import SearchFilter, OrderingFilter
+
+from django_filters import rest_framework as filters
 
 from ...serializers import NewsShortSerializer
 from ...models import News
+
+class NewsFildters(filters.FilterSet):
+
+    class Meta:
+        model = News
+        fields = {
+            'news_type': ['exact'],
+            'publication_date': ['exact', 'gte', 'lte']
+        }
 
 class NewsList(ListAPIView):
     permission_classes = (permissions.AllowAny,)
     queryset = News.objects.all()
     serializer_class = NewsShortSerializer
     pagination_class = PageNumberPagination
-
-    # def get(self, request, first, last):
-    #     news = News.objects.all()[first-1:last]
-    #     serializer = NewsShortSerializer(news, many=True, context={"request":request})
-    #     return Response(serializer.data)
+    filter_backends = (SearchFilter, OrderingFilter, filters.DjangoFilterBackend)
+    filterset_class = NewsFildters
+    search_fields = ('headline', 'news_text', 'news_type__name')
+    ordering_fields = ('id', 'news_type', 'publication_date')
--- backend/views/RestaurantVeiws/RestaurantList.py
+++ backend/views/RestaurantVeiws/RestaurantList.py
@@ -1,13 +1,11 @@
 from rest_framework import status, permissions
-from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.generics import ListAPIView
 from rest_framework.filters import SearchFilter, OrderingFilter
 
 from django_filters import rest_framework as filters
 
-from ...serializers import RestaurantSerializer, RestaurantDetailSerializer
+from ...serializers import RestaurantSerializer 
 from ...models import Restaurant
 
 class RestaurntFilters(filters.FilterSet):
