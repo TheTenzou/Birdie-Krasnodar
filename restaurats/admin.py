7112b894d380428d32b0b6abb2be04d33a1f649b
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2020-11-23 10:06:16 +0300

    Add admin config to restaturants

--- restaurats/admin.py
+++ restaurats/admin.py
@@ -1,3 +1,64 @@
 from django.contrib import admin
+from django.contrib.gis.db import models
+from .models import Restaurant, RestaurantPicture, FoodPicture, Address, FoodType, PriceBracket
+from mapwidgets.widgets import GooglePointFieldInlineWidget
 
-# Register your models here.
+
+class RestaurantPicturesInLine(admin.TabularInline):
+    model = RestaurantPicture
+    extra = 0
+
+
+class FoodPicturesInLine(admin.TabularInline):
+    model = FoodPicture
+    extra = 0
+
+
+class AddressInLine(admin.TabularInline):
+    model = Address
+    extra = 0
+    formfield_overrides = {
+        models.PointField: {"widget": GooglePointFieldInlineWidget}
+    }
+
+
+class RestaurantAdmin(admin.ModelAdmin):
+    inlines = [
+        RestaurantPicturesInLine, 
+        FoodPicturesInLine,
+        AddressInLine # TODO fix map
+    ]
+    list_display = ('name',)
+    list_filter = ['food_type', 'price_bracket']
+    search_fields = ['name', 'description']
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        form.base_fields['name'].label = 'Название'
+        form.base_fields['description'].label = 'Описание'
+        form.base_fields['food_type'].label = 'Тип еды'
+        form.base_fields['price_bracket'].label = 'Ценавой диапозон'
+        form.base_fields['card_picture'].label = 'Кратинка для карточки'
+        form.base_fields['slug'].label = 'url'
+        return form
+
+
+class FoodTypeAdmin(admin.ModelAdmin):
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        form.base_fields['name'].label = 'Название типа'
+        return form
+
+
+class PriceBracketAdmin(admin.ModelAdmin):
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        form.base_fields['name'].label = 'Ценовая группа'
+        return form
+
+
+admin.site.register(PriceBracket, PriceBracketAdmin)
+admin.site.register(FoodType, FoodTypeAdmin)
+admin.site.register(Restaurant, RestaurantAdmin)
