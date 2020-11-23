b28676b2c146060b16b6f1b995c41016f47bbe6b
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2020-11-23 18:40:22 +0300

    Update model and db

--- backend/models.py
+++ backend/models.py
@@ -4,4 +4,4 @@ from django.db import models
 
 # Create your models here.
 class User(AbstractUser):
-    profile_picture = models.ImageField(upload_to='file_storage/profile_pictures')
+    profile_picture = models.FileField(upload_to='file_storage/profile_pictures')
