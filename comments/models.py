7e4c65cbe8cca17db6c32acb84829a3fdab3260f
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2020-11-23 13:38:17 +0300

    Add model for comments

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from users.models import User
+from news.models import News
 
-# Create your models here.
+
+# Коментарий под новостью
+class Comment(models.Model):
+    # пользователь
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # новость
+    news = models.ForeignKey(News, on_delete=models.CASCADE)
+    # текст
+    text = models.TextField(max_length=500)
+    # дата публикации
+    publication_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta():
+        verbose_name = 'Коментарий'
+        verbose_name_plural = 'Коментарии'
