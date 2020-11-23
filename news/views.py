5a896cefe479cdfa74c1130c0070ca73faa28654
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2020-11-23 11:36:29 +0300

    Add views to news

--- news/views.py
+++ news/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from rest_framework import status, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView
+from rest_framework.pagination import PageNumberPagination
 
-# Create your views here.
+from django_filters import rest_framework as filters
+
+from .serializers import NewsTypeSerializer, NewsPicturesSerializer, NewsShortSerializer, NewsSerializer
+from .models import NewsType, NewsPicture, News
+
+class NewsTypeList(APIView):
+    permission_classes = (permissions.AllowAny,)
+
+    def get(self, request):
+        news = NewsType.objects.all()
+        serializer = NewsTypeSerializer(news, many=True, context={"request":request})
+        return Response(serializer.data)
+
+
+class NewsPictureList(APIView):
+    permission_classes = (permissions.AllowAny,)
+
+    def get(self, request):
+        pictures = NewsPicture.objects.all()
+        serializer = NewsPicturesSerializer(pictures, many=True, context={"request":request})
+        return Response(serializer.data)
+
+
+class NewsFildters(filters.FilterSet):
+
+    class Meta:
+        model = News
+        fields = {
+            'news_type': ['exact'],
+            'publication_date': ['exact', 'gte', 'lte']
+        }
+
+
+class NewsList(ListAPIView):
+    permission_classes = (permissions.AllowAny,)
+    queryset = News.objects.all()
+    serializer_class = NewsShortSerializer
+    pagination_class = PageNumberPagination
+    filter_backends = (SearchFilter, OrderingFilter, filters.DjangoFilterBackend)
+    filterset_class = NewsFildters
+    search_fields = ('headline', 'news_text', 'news_type__name')
+    ordering_fields = ('id', 'news_type', 'publication_date')
+
+
+class NewsDetails(APIView):
+    permission_classes = (permissions.AllowAny,)
+
+    def get(self, request, slug):
+        news = News.objects.get(slug=slug)
+        serializer = NewsSerializer(news, many=False, context={"request":request})
+        return Response(serializer.data)
