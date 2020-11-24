e3523992209dc6babe4efbbf644686af1ab68891
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2020-11-24 08:33:45 +0300

    Add update, delete comments

--- comments/urls.py
+++ comments/urls.py
@@ -3,5 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('list/', CommentList.as_view(), name='comments_list'),
-    path('create/', CommentsCreate.as_view(), name='create_list'),
+    path('create/', CommentCreate.as_view(), name='create_list'),
+    path('update/<int:pk>/', CommentUpdate.as_view(), name='update_comment'),
+    path('delete/<int:pk>/', CommentDelete.as_view(), name='delete_comment')
 ]
--- comments/views.py
+++ comments/views.py
@@ -30,7 +30,7 @@ class CommentList(ListAPIView):
     ordering_fields = ('publication_date',)
 
 
-class CommentsCreate(APIView):
+class CommentCreate(APIView):
     permission_classes = (permissions.AllowAny,)
 
     def post(self, request, format='json'):
@@ -42,3 +42,30 @@ class CommentsCreate(APIView):
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CommentUpdate(APIView):
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, pk, format='json'):
+        review = Comment.objects.get(id=pk)
+        serializer = CommentSerializer(instance=review, data=request.data)
+
+        if serializer.is_valid():
+            new_comment = serializer.save()
+            if new_comment:
+                json = serializer.data
+                return Response(json, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CommentDelete(APIView):
+    permission_classes = (permissions.AllowAny,)
+
+    def delete(self, request, pk):
+        if Comment.objects.filter(id=pk).exists():
+            review = Comment.objects.get(id=pk)
+            review.delete()
+            return Response(status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
