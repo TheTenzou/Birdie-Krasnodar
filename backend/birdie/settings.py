008b555c4cbe90f352622478169eb49aae5edfc0
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2021-03-27 22:01:35 +0300

    Dockerized project

--- backend/birdie/settings.py
+++ backend/birdie/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,13 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'pjia4jw9bnm#3=yi)c1gtc!d^h!hsgc=gt$qfwq8*y6@=r3g97'
-
+# SECRET_KEY = 'pjia4jw9bnm#3=yi)c1gtc!d^h!hsgc=gt$qfwq8*y6@=r3g97'
+SECRET_KEY = os.environ.get('SECRET_KEY') 
+ 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = ['*']
-
+# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(' ')
 
 # Application definition
 
@@ -116,13 +119,13 @@ WSGI_APPLICATION = 'birdie.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'ENGINE': os.environ.get('SQL_ENGINE', 'django.db.backends.sqlite3'),
         # TODO: use enviroment variables for database credentials
-        'NAME': 'birdie',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'POST': '5432'
+        'NAME': os.environ.get('SQL_DATABASE', BASE_DIR.joinpath("db.sqlite3")),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
     }
 }
 
